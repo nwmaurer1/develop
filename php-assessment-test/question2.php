50563b6611b5631352d12f6fb311252478c0c078
nwmaurer1 <nicholas.maurer2@gmail.com>
2018-12-31 17:33:08 -0500

    Add Example data. Major Logic clean-up, switch from multisort to usort functionality. Add initial test bench

--- php-assessment-test/question2.php
+++ php-assessment-test/question2.php
@@ -1,74 +1,155 @@
 <?php
 
+$example = array (
+    array (
+        'guest_id' => 177,
+        'guest_type' => 'crew',
+        'first_name' => 'Marco',
+        'middle_name' => NULL,
+        'last_name' => 'Burns',
+        'gender' => 'M',
+        'guest_booking' => array (
+            array (
+                'booking_number' => 20008683,
+                'ship_code' => 'OST',
+                'room_no' => 'A0073',
+                'start_time' => 1438214400,
+                'end_time' => 1483142400,
+                'is_checked_in' => true,
+            ),
+        ),
+        'guest_account' => array (
+            array (
+                'account_id' => 20009503,
+                'status_id' => 2,
+                'account_limit' => 0,
+                'allow_charges' => true,
+            ),
+        ),
+    ),
+    array (
+        'guest_id' => 10000113,
+        'guest_type' => 'crew',
+        'first_name' => 'Bob Jr ',
+        'middle_name' => 'Charles',
+        'last_name' => 'Hemingway',
+        'gender' => 'M',
+        'guest_booking' => array (
+            array (
+                'booking_number' => 10000013,
+                'room_no' => 'B0092',
+                'is_checked_in' => true,
+            ),
+        ),
+        'guest_account' => array (
+            array (
+                'account_id' => 10000522,
+                'account_limit' => 300,
+                'allow_charges' => true,
+            ),
+        ),
+    ),
+    array (
+        'guest_id' => 10000114,
+        'guest_type' => 'crew',
+        'first_name' => 'Al ',
+        'middle_name' => 'Bert',
+        'last_name' => 'Santiago',
+        'gender' => 'M',
+        'guest_booking' => array (
+            array (
+                'booking_number' => 10000014,
+                'room_no' => 'A0018',
+                'is_checked_in' => true,
+            ),
+        ),
+        'guest_account' => array (
+            array (
+                'account_id' => 10000013,
+                'account_limit' => 300,
+                'allow_charges' => true,
+            ),
+        ),
+    ),
+    array (
+        'guest_id' => 10000115,
+        'guest_type' => 'crew',
+        'first_name' => 'Red ',
+        'middle_name' => 'Ruby',
+        'last_name' => 'Flowers ',
+        'gender' => 'F',
+        'guest_booking' => array (
+            array (
+                'booking_number' => 10000015,
+                'room_no' => 'A0051',
+                'is_checked_in' => true,
+            ),
+        ),
+        'guest_account' => array (
+            array (
+                'account_id' => 10000519,
+                'account_limit' => 300,
+                'allow_charges' => true,
+            ),
+        ),
+    ),
+    array (
+        'guest_id' => 10000116,
+        'guest_type' => 'crew',
+        'first_name' => 'Ismael ',
+        'middle_name' => 'Jean-Vital',
+        'last_name' => 'Jammes',
+        'gender' => 'M',
+        'guest_booking' => array (
+            array (
+                'booking_number' => 10000016,
+                'room_no' => 'A0023',
+                'is_checked_in' => true,
+            ),
+        ),
+        'guest_account' => array (
+            array (
+                'account_id' => 10000015,
+                'account_limit' => 300,
+                'allow_charges' => true,
+            ),
+        ),
+    ),
+);
 
 
-function sortDataStructureByKey(&$data, $sortingkeys = array(), $first_pass = true)
+function sortDataStructureByKey($data, $sortingKeys)
 {
-    $temp_array = [];
-    if (is_array($sortingkeys)) {
-        foreach ($sortingkeys as $sortingkey) {
-            echo "<pre>";
-            print_r($temp_array);
-            $temp_array1 = array_column($data, $sortingkey);
-            if (empty($temp_array1)) {
-                $temp_array2 = $temp_array;
-                foreach ($data as $array) {
-                    foreach ($array as $key => $value) {
-                        if (is_array($value)) {
-                            $temp_array[] = sortDataStructureByKey($value, $sortingkey, false);
-                        }
-                    }
-                }
-                echo "<pre>";
-                print_r($temp_array1);
-                $temp_array1 = array_column($temp_array, $sortingkey);
-                echo "<pre>";
-                print_r($temp_array1);
-                $temp_array = array_merge_recursive($temp_array2, $temp_array1);
-                echo "<pre>";
-                print_r($temp_array);
-                array_multisort($temp_array, SORT_ASC, $data);
-            } else {
-                $temp_array = array_merge_recursive($temp_array, $temp_array1);
-                echo "<pre>";
-                print_r($temp_array);
-                array_multisort($temp_array, SORT_ASC, $data);
-            }
-        }
-    } else {
-        if (!$first_pass && array_key_exists($sortingkeys, $data)) {
-            return $data;
-        } else if ($first_pass) {
-            $temp_array = array_column($data, $sortingkeys);
-            if (empty($temp_array)) {
-                foreach ($data as $array) {
-                    foreach ($array as $key => $value) {
-                        if (is_array($value)) {
-                            $temp_array[] = sortDataStructureByKey($value, $sortingkeys, false);
-                        }
-                    }
-                }
-                $temp_array = array_column($temp_array, $sortingkeys);
-                array_multisort($temp_array, SORT_ASC, $data);
-            } else {
-                array_multisort($temp_array, SORT_ASC, $data);
-            }
-        } else {
-            foreach ($data as $child) {
-                if (is_array($child)) {
-                    $temp_array = sortDataStructureByKey($child, $sortingkeys, false);
-                    if (isset($temp_array)) {
-                        return $temp_array;
-                    }
-                }
-            }
-            return [];
-        }
+    if(!is_array($sortingKeys)) {
+        $sortingKeys = array($sortingKeys);
     }
-    echo '<pre>';
-    print_r($data);
-}
+    usort($data, function($a, $b) use($sortingKeys) {
+        $returnVal = 0;
+        foreach($sortingKeys as $sortingKey) {
+            if (!array_key_exists($sortingKey, $a)) {
+                $keys = array_keys($a);
+                foreach ($keys as $key) {
+                    if (is_array($a[$key])) {
+                        foreach ($a[$key] as $k =>$child) {
+                            if (array_key_exists($sortingKey, $child)) {
+                                if($returnVal == 0) {
+                                    $returnVal = strnatcmp($a[$key][$k][$sortingKey],$b[$key][$k][$sortingKey]);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                if($returnVal == 0) {
+                    $retval = strnatcmp($a[$sortingKey],$b[$sortingKey]);
+                }
+            }
 
+        }
+        return $retval;
+    });
+    return $data;
+}
 
 
 $data = sortDataStructureByKey($example, ['room_no','account_id']);
@@ -78,4 +159,3 @@ print_r($data);
 
 echo "\n\n";
 print_r($data1);
-//sortDataStructureByKey($example, ['last_name', 'account_id']);
